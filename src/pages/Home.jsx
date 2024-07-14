2e96a6e9c1ad28c47186ea7028f434a48ba583c6
yuran-nhassengo <yurannhassengo@gmail.com>
2024-07-14 17:48:22 +0200

    Criando função para filtar dados

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,17 @@ import { Nav } from '../Navigation/Nav'
 import { SideBar } from '../Sidebar/sideBar'
 
 //Database
-import  products  from '../data/db'
+import  products  from '../db/data'
+import { Card } from '../components/Card'
 
 export const Home = () => {
 
   const [selectedCategory,setSelectedCategory] = useState(null);
+  const [query,setQuery] = useState("")
+
 
   //--------------Input Filter----------------
-  const [query,setQuery] = useState("")
+  
 
   const handleInputChange = event =>{
     setQuery(event.target.value);
@@ -24,7 +27,57 @@ export const Home = () => {
   const filteredItems = products.filter((product) => 
     product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1));
 
-  //--------------Input Filter----------------
+  //--------------Radio Filter----------------
+  const handleChange = event => {
+    setSelectedCategory(event.target.value)
+  }
+
+//--------------Buttons Filter----------------
+
+  const handleClick = event =>{
+    setSelectedCategory(event.target.value)
+  }
+
+  function filteredData(products,selected,query){
+
+    let filteredProducts = products
+    
+    //Filtering Input Items
+    if(query){
+      filteredProducts = filteredItems
+    }
+
+    //Selected Filter
+
+    if(selected){
+      filteredProducts =filteredProducts.filter(
+        ({category,color,company,newPrice,title} ) => 
+          category === selected ||
+         color === selected || 
+         company === selected || 
+         newPrice === selected ||
+         title === selected )
+    }
+
+    return filteredProducts.map(({img,title,star,reviews,prevPrice,newPrice}) =>(
+      <Card 
+      key={Math.random()}
+      img={img}
+      title={title}
+      star={star}
+      review={reviews}
+      prevPrice={prevPrice}
+      newPrice={newPrice}
+      
+      
+      />
+    ))
+
+ 
+
+  }
+
+  const result =    filteredData(products,selectedCategory,query);
 
   return (
     <div>
